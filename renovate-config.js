06140c82a503b86ce58eb702900c4351fa2f36ae
Mikael Olausson <104485047+mikaelol@users.noreply.github.com>
2022-12-20 19:29:15 +0100

    renovate config, real

---
+++ renovate-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  branchPrefix: 'test-renovate/',
+  dryRun: true,
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: true,
+  platform: 'github',
+  includeForks: true,
+  repositories: [
+    'mikaelol/test-renovate-upgrades',
+  ],
+};
